852f3e4dd7652a29a9e157c2acf2537bf64f3c29
aditya2133 <adi.pande@gmail.com>
2022-06-09 17:31:31 +0100

    commited data and schemas directly

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,6 +1,6 @@
-INSERT INTO customers Values (1, "Adi", "Pan"),(2, "Sam", "Noll"),(3, "Kate", "Hund"),(4, "Chris", "Hem");
+INSERT INTO customers Values (1, 'Adi', 'Pan'),(2, 'Sam', 'Noll'),(3, 'Kate', 'Hund'),(4, 'Chris', 'Hem');
 
-INSERT INTO items Values(1, "Bat", 40), (2, "Ball", 13), (3, "stumps", 15), (4, "helmet", 40);
+INSERT INTO items Values(1, 'Bat', 40), (2, 'Ball', 13), (3, 'stumps', 15), (4, 'helmet', 40);
 
 INSERT INTO orders Values (1,2),(2,4),(3,1),(4,2),(5,4),(6,2),(7,3),(8,4),(9,1),(10,3), (11,3);
 
